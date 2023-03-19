12d1926b4aae4b67ba6fe376d11f505528d99a56
MAITAYORMARIS <yormarism@gmail.com>
2023-03-19 10:53:46 -0300

    primer commit amazing

---
+++ javascript/AmazingEvents.js
@@ -0,0 +1,92 @@
+var dataAmazing = {
+  "fechaActual": "2022-01-01",
+  "eventos": [
+    {
+      "id": 1,
+      "image": "Feria de comidas7.jpg",
+      "name": "Fiesta de las colectividades",
+      "date": "2021-12-12",
+      "description": "Disfruta de tus platos favoritos, de los diferentes paises, en un evento unico para toda la familia",
+      "category": "Feria de Comida",
+      "place": "Salón A",
+      "capacity": 45000,
+      "assistance": 42756,
+      "price": 5,
+      "info":"Más de 50 culturas estarán presentes en la Fiesta de las Colectividades, el encuentro será desde las 12: 00 hasta las 18: 00 horas. Será una jornada para vivir la multiculturalidad a través de la gastronomía, artesanía, música y danzas típicas de las comunidades extranjeras. Este evento busca alentar el reconocimiento y la apreciación de la diversidad cultural aportada por las colectividades dentro de la identidad de la Ciudad. En los stands podrás degustar una gran variedad de platos típicos: el famoso shawarma armenio,ramen y baos, strudel, variedades de ahumados, entre otros. También estarán presentes los sabores latinoamericanos, como el ceviche colombiano, feijoada y brigadeiros brasileños, entre otras delicias.En los puestos artesanales podrás disfrutar de: exhibición de vestimentas tradicionales, fotos y videos acerca de la historia de sus comunidades en Buenos Aires, experiencia de caligrafía coreana, artesanías celtas, y mucho más.",
+    },
+
+    {
+      "id": 2,
+      "image": "Salida al museo5.jpg",
+      "name": "Parque Juracico",
+      "date": "2021-11-02",
+      "description": "Vamos al museo de paleontologia a hacer un recorrido increible para conocer los mas grandes dinosaurios",
+      "category": "Salida al museo",
+      "place": "Campo",
+      "capacity": 82000,
+      "assistance": 65892,
+      "price": 15,
+      "info":"Esta salida propone un recorrido por la historia de la vida en el planeta, con la exhibición de material paleontológico y arqueológico rescatado.  ",
+    },
+    {
+      "id": 3,
+      "image": "Fiesta de disfraces1.jpg",
+      "name": "Noche de Halloween",
+      "date": "2022-02-12",
+      "description": "Ven con tu personaje disfras mas aterrador para ganar increibles premios",
+      "category": "Fiesta de Disfraces",
+      "place": "Salón C",
+      "capacity": 12000,
+      "estimate": 9000,
+      "price": 12
+    },
+    {
+      "id": 4,
+      "image": "Concierto de musica1.jpg",
+      "name": "Metallica in concert",
+      "date": "2022-01-22",
+      "description": "Unico recital de la banda mas emblematica ",
+      "category": "Concierto de Música",
+      "place": "Salón A",
+      "capacity": 138000,
+      "estimate": 138000,
+      "price": 150
+    },
+    {
+      "id": 5,
+      "image": "Maraton3.jpg",
+      "name": "10K por la vida",
+      "date": "2021-03-01",
+      "description": "Ven y haz ejercicio, mejora tu salud y tu estilo de vida",
+      "category": "Carrera",
+      "place": "Campo de Futból",
+      "capacity": 30000,
+      "assistance": 25698,
+      "price": 3
+    },
+    {
+      "id": 6,
+      "image": "Libros7.jpg",
+      "name": "Feria del libro Escolar",
+      "date": "2021-10-15",
+      "description": "Trae tu libro escolar que ya no usas y lleva el que necesites",
+      "category": "Intercambio de Libros",
+      "place": "Salón D1",
+      "capacity": 150000,
+      "assistance": 123286,
+      "price": 1
+    },
+    {
+      "id": 7,
+      "image": "Cine7.jpg",
+      "name": "Avengers",
+      "date": "2022-10-15",
+      "description": "Premier en 3d de Avengers de Marvel el inicio de una saga epica con tus mejores superheroes",
+      "category": "Vamos al Cine",
+      "place": "Salón D1",
+      "capacity": 9000,
+      "estimate": 9000,
+      "price": 250
+    }
+  ]
+}
